8a44210db3d954016da3bec46cd7512496eaf8f8
Kirill Romanov <workrkm@mail.ru>
2024-05-01 14:23:55 +0300

    feature: сделать страницу 404

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,20 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed, NotFound404 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
 
+import { Navigate, Route, Routes } from 'react-router-dom';
+
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    {/* <ConstructorPage /> */}
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
   </div>
 );
 
