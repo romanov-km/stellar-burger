d0d230768a528f59dd034131a30a4231652e6018
Kirill Romanov <workrkm@mail.ru>
2024-06-22 11:21:52 +0300

    fix: исправить стили кнопок в шапке

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -8,6 +8,7 @@ import {
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
 import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
   const navigate = useNavigate();
@@ -24,30 +25,65 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
         <div className={styles.menu_part_left}>
-          <>
-            <BurgerIcon type={'primary'} />
-            <p
-              className='text text_type_main-default ml-2 mr-10'
-              onClick={openMain}
-            >
-              Конструктор
-            </p>
-          </>
-          <>
-            <ListIcon type={'primary'} />
-            <p className='text text_type_main-default ml-2' onClick={openFeed}>
-              Лента заказов
-            </p>
-          </>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              isActive ? styles.link_active : styles.link
+            }
+          >
+            {({ isActive }) => (
+              <>
+                <BurgerIcon type={'primary'} />
+                <p
+                  className='text text_type_main-default ml-2 mr-10'
+                  onClick={openMain}
+                >
+                  Конструктор
+                </p>
+              </>
+            )}
+          </NavLink>
+          <NavLink
+            to='/feed'
+            className={({ isActive }) =>
+              isActive ? styles.link_active : styles.link
+            }
+          >
+            {({ isActive }) => (
+              <>
+                <ListIcon type={'primary'} />
+                <p
+                  className='text text_type_main-default ml-2'
+                  onClick={openFeed}
+                >
+                  Лента заказов
+                </p>
+              </>
+            )}
+          </NavLink>
         </div>
         <div className={styles.logo}>
           <Logo className='' />
         </div>
         <div className={styles.link_position_last}>
-          <ProfileIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2' onClick={openProfile}>
-            {userName || 'Личный кабинет'}
-          </p>
+          <NavLink
+            to={userName ? '/profile' : '/login'}
+            className={({ isActive }) =>
+              isActive ? styles.link_active : styles.link
+            }
+          >
+            {({ isActive }) => (
+              <>
+                <ProfileIcon type={'primary'} />
+                <p
+                  className='text text_type_main-default ml-2'
+                  onClick={openProfile}
+                >
+                  {userName || 'Личный кабинет'}
+                </p>
+              </>
+            )}
+          </NavLink>
         </div>
       </nav>
     </header>
